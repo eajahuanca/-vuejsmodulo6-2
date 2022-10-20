9b16893e79bcb79b797794cb1419b02ce8a31fb3
Edwin Ajahuanca Callisaya <edwinajahuancacallisaya@gmail.com>
2022-10-19 23:49:00 -0400

    proyecto4

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--<HelloWorld msg="Bienvenido a VueJS3"/>-->
+  <Practica2/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Practica2 from './components/Practica2.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Practica2
   }
 }
 </script>
